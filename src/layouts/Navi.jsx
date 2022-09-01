1130941e78621ae1a8ea615b2502a918c8f96ba9
emreguven72 <emreguven_72@hotmail.com>
2022-09-01 17:29:56 +0300

    9/1/22 5.29 before productService

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -5,7 +5,7 @@ import CartSummary from "./CartSummary";
 export default function Navi() {
   return (
     <div>
-      <Menu inverted fixed>
+      <Menu inverted fixed="top">
         <Container>
           <Menu.Item name="home" />
           <Menu.Item name="messages" />
